7700df891bec8151b6fff9a1915746baf67a08e8
hovhannes-13 <hovo.harutyunyan90@gmail.com>
2026-06-13 19:27:59 +0400

    Update Apps Script URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Replace with your deployed Apps Script Web App URL
-const API_URL = 'https://script.google.com/macros/s/AKfycbzvG8NSQ-hWIz-uzH00EX0nBJeyzMInD0oBGggH7rzS853qD2nVHajMcnIQqIK0VXVO/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycby2Tn5jbIpYHI50PyRFlA2aTFtD61Rg00dkJZ9nZbIZp_bNICqI6TojzOAqk3kDBQxu/exec';
