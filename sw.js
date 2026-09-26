a1fce2ba1fddfb50d26b7cccb31fa2f2b0cc76e7
hovhannes-13 <hovo.harutyunyan90@gmail.com>
2026-09-26 17:46:13 +0400

    Deploy Finances 2026-09-26 17:46

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'finances-arm-v93';
+const CACHE = 'finances-arm-v94';
 
 const ASSETS = [
   './',
   './index.html',
-  './style.css?v=108',
+  './style.css?v=109',
   './app.js?v=141',
   './exports.js?v=1',
   './config.js?v=22',
